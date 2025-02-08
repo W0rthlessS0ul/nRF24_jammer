90b85fa38c1d3bc35dd2a228ed3feaee8ee707db
W0rthlessS0ul <nevagno.real@gmail.com>
2025-02-08 20:13:49 +0300

    New Improvements
    Added the ability to exit the jamming mode by pressing the OK button (25 pin).

--- src/128x64_Compact/nRF24_jammer/jam.h
+++ src/128x64_Compact/nRF24_jammer/jam.h
@@ -9,10 +9,13 @@ void HSPI_init() {
   radio.setDataRate(RF24_2MBPS);
   radio.setCRCLength(RF24_CRC_DISABLED);
   radio.startConstCarrier(RF24_PA_MAX, 40); 
+  hspi = true;
 }
 void bluetooth_jam(){
-  HSPI_init();
-  while (true){
+  if (hspi == false) HSPI_init();
+  butt1.tick();
+  while (!butt1.isSingle()){
+    butt1.tick();
     if (bluetooth_jam_method == 0){
       for (int i = 0; i < 22; i++) {
         radio.setChannel(bluetooth_channels[i]);
@@ -30,8 +33,10 @@ void bluetooth_jam(){
   }
 }
 void drone_jam(){
-  HSPI_init();
-  while (true){
+  if (hspi == false) HSPI_init();
+  butt1.tick();
+  while (!butt1.isSingle()){
+    butt1.tick();
     if (drone_jam_method == 0){
       int random_channel = random(125);
       radio.setChannel(random_channel);
@@ -44,47 +49,62 @@ void drone_jam(){
   }
 }
 void ble_jam(){
-  HSPI_init();
-  while (true){
+  if (hspi == false) HSPI_init();
+  butt1.tick();
+  while (!butt1.isSingle()){
+    butt1.tick();
     for (int i = 0; i < 3; i++){
       radio.setChannel(ble_channels[i]);
     }
   }
 }
 void wifi_jam(){
-  HSPI_init();
-  while (true){
+  if (hspi == false) HSPI_init();
+  butt1.tick();
+  while (!butt1.isSingle()){
     for (int channel = 0; channel < 13; channel++) {
       for (int i = (channel * 5) + 1; i < (channel * 5) + 23; i++) {
+        butt1.tick();
+        if (butt1.isSingle()) break;
         radio.setChannel(i);
         radio.writeFast(&jam_text, sizeof(jam_text));
       }
+      if (butt1.isSingle()) break;
     }
   }
 }
 void wifi_channel(int channel){
-  HSPI_init();
-  while (true){
+  if (hspi == false) HSPI_init();
+  butt1.tick();
+  while (!butt1.isSingle()){
     for (int i = (channel * 5) + 1; i < (channel * 5) + 23; i++) {
+      butt1.tick();
+      if (butt1.isSingle()) break;
       radio.setChannel(i);
       radio.writeFast(&jam_text, sizeof(jam_text));
     }
   }
 }
 void zigbee_jam(){
-  HSPI_init();
-  while (true){
+  if (hspi == false) HSPI_init();
+  butt1.tick();
+  while (!butt1.isSingle()){
     for (int channel = 11; channel < 27; channel++){
       for (int i = 5+5*(channel-11); i < (5+5*(channel-11))+6; i++){
+        butt1.tick();
+        if (butt1.isSingle()) break;
         radio.setChannel(i);
         radio.writeFast(&jam_text, sizeof(jam_text));
       }
+      if (butt1.isSingle()) break;
     }
   }
 }
 void misc_jam(int channel1, int channel2){
-  HSPI_init();
-  while (true) {
+  if (hspi == false) HSPI_init();
+  butt1.tick();
+  while (!butt1.isSingle()){
+    butt1.tick();
     for (int i = 0; i <= channel2 - channel1; i++) {
       radio.setChannel(channel1 + i);
       if (misc_jam_method == 1){
