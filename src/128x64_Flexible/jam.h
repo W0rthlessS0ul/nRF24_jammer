a91b855622a8e53ba25a7bd0efbab8c78668112c
W0rthlessS0ul <nevagno.real@gmail.com>
2025-08-29 11:30:02 +0300

    new version (2.6.0)
    Added a new firmware version called "Flexible"

---
+++ src/128x64_Flexible/jam.h
@@ -0,0 +1,15 @@
+#ifndef JAM_H
+#define JAM_H
+
+#include "options.h"
+
+void HSPI_init();
+void bluetooth_jam();
+void drone_jam();
+void ble_jam();
+void wifi_jam();
+void wifi_channel(int channel);
+void zigbee_jam();
+void misc_jam(int channel1, int channel2);
+
+#endif
